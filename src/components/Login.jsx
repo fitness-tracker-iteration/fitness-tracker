68e2e3b9f2526a316e4134813e25217aa918fa49
Victor Martins <martinsvictor287@gmail.com>
2023-08-02 21:09:13 +0300

    refactor login UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,55 +1,80 @@
+import { FormControl, FormLabel } from '@mui/material';
 import React, { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate()
-  const [isSuccess, setisSuccess] = useState(true)
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
+	const [isSuccess, setisSuccess] = useState(true);
 
+	const handleLoginClick = async () => {
+		// if (loginSuccess) {
+		//   setisSuccess(!isSuccess)
 
-  const handleLoginClick = async () => {
-    // if (loginSuccess) {
-    //   setisSuccess(!isSuccess)
+		//   navigate('/main');
+		// }
+		try {
+			const response = await fetch('/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username: username, password: password }),
+			});
+			const data = await response.json();
+			if (data.authenticated) navigate('/main');
+		} catch (error) {
+			console.log(error);
+			setisSuccess(false);
+		}
+	};
 
-    //   navigate('/main');
-    // }
-    try {
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: username, password: password })
-      })
-      const data = await response.json();
-      if (data.authenticated)
-        navigate('/main');
-    } catch (error) {
-      console.log(error);
-      setisSuccess(false);
-    }
-  }
+	const handleSignupClick = () => {
+		navigate('/signup');
+	};
 
-  const handleSignupClick = () => {
-    navigate('/signup');
-  }
-
-  return (
-    <div>
-      <div className="login-container">
-        <h1 className='login-text'>WEIGHT LOSS APP!!!!!</h1>
-        <img className='login-pic' src="https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/49067/large_thumb%402x.png"></img>
-        <input className='input-fields' type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username...' ></input>
-        <input className='input-fields' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password...' ></input>
-        <div className="login-button-container">
-          <button className="login-buttons" id="login-button" onClick={handleLoginClick}>Log In</button>
-          <button className="login-buttons" id="signup-button" onClick={handleSignupClick}>Sign Up</button>
-        </div>
-      </div>
-      {!isSuccess && <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', color: 'red' }}>Login credentials not found</p>}
-    </div>
-  )
-}
+	return (
+		<div>
+			<div>
+				<Typography variant="h4" gutterBottom>
+					Log in to your account
+				</Typography>
+				<Stack spacing={3}>
+					<FormControl>
+						<FormLabel>Username</FormLabel>
+						<TextField
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							placeholder="Username..."
+						></TextField>
+					</FormControl>
+					<FormControl>
+						<FormLabel>password</FormLabel>
+						<TextField
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							placeholder="Password..."
+						></TextField>
+					</FormControl>
+					<Button onClick={handleLoginClick} variant="contained">
+						Log In
+					</Button>
+					<Typography variant="subtitle1" gutterBottom>
+						Already got an account? Sign up
+					</Typography>
+				</Stack>
+			</div>
+		</div>
+	);
+};
 
 export default Login;
