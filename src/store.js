d7e7beac625c12e6ce736c1298513a7d73018b15
Jordan Querubin <jordan@Jordans-MacBook-Pro.local>
2023-08-02 10:27:49 -0700

    made notes on toolkit logic and notes on dashboard changes/revisions

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { statSlice } from "./reducer";
 
+/** store.js allows us to create multiple slices that handle different states if needed. In our case, we only need one. */
 const store = configureStore({
     reducer: {
         stats: statSlice.reducer
     }
 });
 
-
+/** This store is being imported inside index.js and being passed as a property within the Provider component thats
+ *  wrapping the App component. This allows all components within App to use the reducers within store.
+ */
 export default store;
